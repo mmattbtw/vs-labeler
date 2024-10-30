2ca3fbee47819fa9b075ea7c675c65ae5a550bd4
mmattbtw <matt@mmatt.net>
2024-10-30 15:26:30 -0500

    yep

--- index.ts
+++ index.ts
@@ -1,4 +1,8 @@
+import { Bot, Post } from "@skyware/bot";
 import { LabelerServer } from "@skyware/labeler";
+
+// pathselector: at://did:plc:eivabf3dhre6rtqs2tkyj6f2/app.bsky.feed.post/3l7qxpakwwn22
+// technicangel: at://did:plc:eivabf3dhre6rtqs2tkyj6f2/app.bsky.feed.post/3l7qxpbmgab2f
 
 const server = new LabelerServer({
   did: process.env.LABELER_DID ?? "",
@@ -10,5 +14,28 @@ server.start(14831, (error) => {
     console.error("Failed to start server:", error);
   } else {
     console.log("Labeler server running on port 14831");
+  }
+});
+
+const bot = new Bot();
+await bot.login({
+  identifier: process.env.LABELER_DID ?? "",
+  password: process.env.LABELER_PASSWORD ?? "",
+});
+
+const postsToLabels: Record<string, string> = {
+  "at://did:plc:eivabf3dhre6rtqs2tkyj6f2/app.bsky.feed.post/3l7qxpakwwn22":
+    "pathselector",
+  "at://did:plc:eivabf3dhre6rtqs2tkyj6f2/app.bsky.feed.post/3l7qxpbmgab2f":
+    "technicangel",
+};
+
+bot.on("like", async ({ subject, user }) => {
+  if (subject instanceof Post) {
+    console.log(subject.uri);
+    const label = postsToLabels[subject.uri];
+    if (label) {
+      await user.labelAccount([label]);
+    }
   }
 });
